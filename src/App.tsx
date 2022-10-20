78e9b455c4900bb9370dffc2af3e937e3936fb51
michaltrabski <32506857+michaltrabski@users.noreply.github.com>
2022-10-20 13:34:12 +0200

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+import "./styles.css";
+
+const taskUrl =
+  "https://www.testdome.com/questions/react-js/focusable-input/85204";
+
+const FocusableInput = (props: any) => {
+  // Write your code heressfrwer
+
+  const inputRef = React.useRef(null);
+
+  React.useEffect(() => {
+    if (props.shouldFocus) {
+      // inputRef.current?.focus();
+    }
+  }, []);
+
+  return <input className="xxx" ref={inputRef} />;
+};
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>TestDome</h1>
+      <p>
+        Task name: <strong>Focusable Input</strong>
+      </p>
+      <p>
+        Task Description: <strong>lorem..</strong>
+      </p>
+      <p>
+        Task source: <strong>{taskUrl}</strong>
+      </p>
+      <p>This is component with solution:</p>
+
+      <FocusableInput shouldFocus={true} />
+    </div>
+  );
+}
