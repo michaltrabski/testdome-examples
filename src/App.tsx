5a768f44fb5960ca92d05093a1d5da88ea8888ef
michaltrabski <32506857+michaltrabski@users.noreply.github.com>
2022-10-20 13:44:54 +0200

    Focusable Input - update 1

--- src/App.tsx
+++ src/App.tsx
@@ -5,33 +5,39 @@ const taskUrl =
   "https://www.testdome.com/questions/react-js/focusable-input/85204";
 
 const FocusableInput = (props: any) => {
-  // Write your code heressfrwer
+  // Write your code here
 
-  const inputRef = React.useRef(null);
+  const inputRef = React.useRef<HTMLInputElement | null>(null);
 
   React.useEffect(() => {
     if (props.shouldFocus) {
-      // inputRef.current?.focus();
+      inputRef.current?.focus();
     }
-  }, []);
+  }, [props.shouldFocus]);
 
-  return <input className="xxx" ref={inputRef} />;
+  return <input ref={inputRef} />;
 };
 
 export default function App() {
   return (
     <div className="App">
-      <h1>TestDome asd</h1>
+      <h1>TestDome</h1>
       <p>
-        asdasd Task name: <strong>Focusable Input</strong>
+        Task name: <strong>Focusable Input</strong>
       </p>
       <p>
-        Task Description: <strong>lorem..</strong>
+        <strong>Task Description:</strong>
       </p>
       <p>
-        Task source: <strong>{taskUrl}</strong>
+        Finish the FocusableInput component so that the input element
+        automatically receives focus on the first render if the shouldFocus prop
+        is true. The component should use React Hooks. HTML5, CSS3, React v16
+        (available as React and ReactDOM)
       </p>
-      <p>This is component with solution:</p>
+      <p>
+        Task source: <a href={taskUrl}>{taskUrl}</a>
+      </p>
+      <p>This input should have focus on page load.</p>
 
       <FocusableInput shouldFocus={true} />
     </div>
